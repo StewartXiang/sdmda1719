87a6a7f7dd9c31b75fe0c57f21c13da44cd2a9ec
StewartXiang <37233775+StewartXiang@users.noreply.github.com>
2019-01-26 15:43:32 +0800

    nnnn

---
+++ Scripts/weasel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weasel : MyObject
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnEnable()
+    {
+        mySpeed = Vector2.zero;
+    }
+    public void IntroSpeed(Vector2 target)
+    {
+        this.mySpeed = (target - new Vector2(this.transform.position.x, this.transform.position.y));
+    }
+}
